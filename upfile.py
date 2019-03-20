770acb2fb2f092536451a76afa54af8495fbddb6
AWP-Dean <sxzbzt@qq.com>
2019-03-20 13:39:40 +0800

    来文来电上传至服务器端修正完毕 排除‘~$2019.xlsx’文件

--- upfile.py
+++ upfile.py
@@ -1,5 +1,5 @@
 import os, shutil
-import datetime
+import time
 
 
 #从本机上传至服务器端
@@ -10,38 +10,36 @@ def backupfile(src_path, dst_path):
         return
     dir_file_list = os.listdir(src_path) #获取所有目录，文件
     for name in dir_file_list:
-        print(name)
-        if name == 'RECYCLER' \
-                or name == 'Thumbs.db'\
-                or name == 'System Volume Information'\
-                or name == '来文来电':
+        if name == '~$2019.xlsx':
             pass
-        else:
-            abs_path = os.path.join(src_path,name) #拼接绝对路径
-            if os.path.isfile(abs_path):
-                #print('这是文件%s'%abs_path)
-                start_time = datetime.datetime.now()
-                print(start_time, " 开始复制……")
-                print('源文件是：',abs_path)
-                back_path = os.path.join(dst_path, src_path[20:])#abs_path[20:]去掉前面的‘\\192.168.3.222\服务器\’
-                print('目标文件夹：',back_path)
-                if not os.path.exists(back_path):
-                    os.makedirs(back_path)
-                back_file = back_path + '\\' + name
-                print('目标文件', back_file)
-                shutil.copyfile(abs_path, back_file)
-                print(abs_path, ' => ', back_file, 'copy done!')
+        else :
+            srcfile_path = os.path.join(src_path, name)
+            backfile_path = os.path.join(dst_path, name)
+            loggerprint('源文件：%s'%(srcfile_path))
+            loggerprint('目标文件:%s'%(backfile_path))
+            shutil.copyfile(srcfile_path, backfile_path)
+            loggerprint('%s => %s copy done!'%(srcfile_path,backfile_path))
 
-            if os.path.isdir(abs_path):
-                #print('这是目录%s'%abs_path)
-                backupfile(abs_path,dst_path)  #如果是目录，则继续递归执行函数
+def time1():
+    return time.strftime("%Y-%m-%d",time.localtime())
 
+def currenttime():
+    return time.strftime("%Y-%m-%d  %H:%M:%S",time.localtime())
 
+def loggerprint(str):
+	filepath = r'C:\Users\Administrator\Desktop\来文来电\\logger\\'
+	filename = time1() + '.txt'
+	log = open(filepath + filename, 'a',newline='')
+	print(currenttime(), file=log)
+	print('\r\n', file=log)
+	print(str, file=log)
+	print('\r\n',file=log)
+	log.close()
 
 
 if __name__ == "__main__":
-    src_path = r'\\192.168.3.222\服务器'
-    dst_path = r'd:\backupfile'
+    dst_path = r'\\192.168.3.222\服务器\来文来电'
+    src_path = r'C:\Users\Administrator\Desktop\来文来电'
     if not os.path.exists(dst_path):
         os.makedirs(dst_path)
     backupfile(src_path,dst_path)
